a9e74312f499622de81f5c505e9f5b374a03102f
ngocphu <ngocphupham682001@gmail.com>
2022-02-01 17:29:59 +0700

    useeffect

--- 4.Hook/use-effect/src/Content.js
+++ 4.Hook/use-effect/src/Content.js
@@ -1,6 +1,9 @@
 
 import { useEffect } from "react";
 import { useState } from "react";
+
+
+/////////////////// Call API ////////////////
 // Call back luôn được gọi sau khi component mounted 
 
  /*1.useEffect(callback)
@@ -11,18 +14,26 @@ import { useState } from "react";
  3.useEffect(callback,[dependency])
  // callback sẽ được gọi lại khi dependency thay đởi 
  */
+
+
+ ///////////// LISTTEN DOM EVENT////////////////
+ // Scroll lắng nghe cuộn trang 
+ // Resize 
+
 const tab = ['posts','comments','albums']
 function Content()
 {
     const [title,setTitle] = useState("")
     const [posts,setPosts] = useState([]);
     const [type,setType] = useState("posts");
+    const[showGotoTop,setShowGotoTop] = useState(false)
     console.log(type)
     useEffect(()=>
     {
         document.title = title
      
     })
+    // call api đặt dữ liệu vào 
     useEffect(()=>{
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
         .then( res => res.json())
@@ -32,6 +43,24 @@ function Content()
         })
     },[type])
 
+    useEffect(()=>{
+        const handleScroll = () => {
+            //khoảng cách cuộn xuống dưới bao nhiêu pixel 
+           if(window.scrollY >= 200)
+           {
+               // show 
+               setShowGotoTop(true)
+           }
+           else
+           {
+               //hide
+               setShowGotoTop(false)
+           }
+        }
+        // dùng effect dạng 2 
+        // add event listener 
+        window.addEventListener ("scroll",handleScroll)
+    })
  
     return (
         <div>
@@ -59,12 +88,17 @@ function Content()
                         {el.title || el.name}
                     </li>
                 ))}
-      
+                
+                {showGotoTop && 
+                <button style = {{
+                    position: 'fixed',
+                    right : 10 ,
+                    bottom : 20 
+                }}>Go to show top </button>}
         </ul>
-        </div>
+  
+    //     </div>
     ) 
    
-  
-
 }
 export  default Content; 
