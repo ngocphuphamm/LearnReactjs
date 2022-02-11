b55511a455061988192d5b21a5d20e80ad4fd3fd
ngocphu <ngocphupham682001@gmail.com>
2022-02-12 00:05:39 +0700

    use-imperative-handle

---
+++ 4.Hook/use-imperative-handle/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { useImperativeHandle} from 'react'
+import Video from './Video'
+function App() {
+  return (
+      <div>
+        <Video />
+      </div>
+  );
+}
+
+export default App;
---
+++ 4.Hook/use-imperative-handle/src/Video.js
@@ -0,0 +1,9 @@
+import video1 from  './video/video1.mp4'
+
+function Video(){
+    return (
+        <video src ={video1}/>
+    )
+}
+
+export default Video ; 
