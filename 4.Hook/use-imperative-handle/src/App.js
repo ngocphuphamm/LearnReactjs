f3061d1288cccba7fe408858bd06a4d314537cd9
ngocphu <ngocphupham682001@gmail.com>
2022-02-12 01:18:33 +0700

    css

--- 4.Hook/use-imperative-handle/src/App.js
+++ 4.Hook/use-imperative-handle/src/App.js
@@ -1,11 +1,47 @@
-
 import './App.css';
-import { useImperativeHandle} from 'react'
+import {useRef,useEffect} from 'react'
 import Video from './Video'
+
+// useImperativehandle (ref, function return object)
+// dùng để bảo toàn dữ liệu bên trong các component khi mà dùng ref  tới
+// component của nó thì khi ta dùng ref tham chiếu thẳng đến component của nó
+// thì nó sẽ mất đi tính bảo mật dữ liệu vì hacker có thể thay đổi thuộc tính của dữ liệu bát cứ lúc nào 
+// Thì với useImperativehandle nó chỉ cho ref vào các phương thức mà request đến component nhất định tránh hacker
+
+
+
 function App() {
+  // sử dụng ref để  có thực hiện function play và pause 
+  // ref này  để tham chiếu qua component video 
+  const videoRef =  useRef(); 
+  
+
+  useEffect(()=>{
+    // hiển thi ref này hiện tại đang ở component nào 
+    // console.log ra component bên video 
+    console.log(videoRef);
+  })
+
+  // sử dụng function xử lý
+  const handlePlay = ()=>{
+    // current là 1 atribute trong ref vì ref sẽ quăng ra 1 đối tượng
+    // play này gọi bên play video
+    videoRef.current.play();
+  }
+
+  
+  // sử dụng function xử lý
+  const handleStop = ()=>{
+    // current là 1 atribute trong ref vì ref sẽ quăng ra 1 đối tượng
+    // pause này được gọi bên video 
+    videoRef.current.pause();
+  }
+
   return (
       <div>
-        <Video />
+        <Video  ref= {videoRef}/>
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={handleStop}>Pause</button>
       </div>
   );
 }
--- 4.Hook/use-imperative-handle/src/Video.js
+++ 4.Hook/use-imperative-handle/src/Video.js
@@ -1,9 +1,37 @@
 import video1 from  './video/video1.mp4'
+import {forwardRef,useImperativeHandle,useRef} from 'react'
 
-function Video(){
+// forward ref  để sử dụng ref trong component 
+// để lấy được ref thông qua forward ref
+// thì function nameFunction (props, ref)
+function Video(props,ref){
+    const videoRef = useRef()
+    // nên dùng useimper để có tính đóng gói 
+    // cái callback được gọi ref ở component app bên app và ta truyền tham chiếu 
+    // videoRef của component hiện tại vào thông qua như midleware v
+    // open ra ngoài 2 method 
+    useImperativeHandle(ref,()=>(
+        {
+        // hàm phương thức thực hiện 
+        play() {
+                videoRef.current.play()
+        },
+        pause(){
+            videoRef.current.pause()
+        }
+        }
+    ))
     return (
-        <video src ={video1}/>
+        <video 
+           
+            // ref mới trong videoref
+            ref = {videoRef}
+            src  ={video1}
+            width = "400px"
+        />
+
+        
     )
 }
-
-export default Video ; 
+// nhận ref 
+export default forwardRef(Video) ; 
